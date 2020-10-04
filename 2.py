6c1fd08f81fc9e922b654ff4a3d4c6be630fb697
CyrilZxy <54348995+CyrilZxy@users.noreply.github.com>
2020-10-04 22:40:06 +0800

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,72 @@
+#变量
+message = "Hello Python world"
+print(message)
+
+message = "hello python"
+print(message)
+
+mesage = "python"
+print(mesage)
+
+
+#字符串
+print("this is a string")
+print('string')
+
+name = "cYril zXy"
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = ("cyril")
+last_name = ("zxy")
+full_name = (first_name+" "+last_name)
+print("Hello"+" "+full_name.title()+" !")
+message=("Hello"+" "+full_name.title()+" !")
+print(message)
+
+print("python")
+print("\tpython")
+print("python1\npython2\n")
+print("制表和换行：\n\t测试1\n\t测试2\n\t测试3")
+
+favorite_language = " python "
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+
+message = "zxy's book"
+print(message)
+
+
+#数字
+#shell中运行
+age=23
+message="happy "+str(age)+"rd"
+print(message)
+print(5+3)
+print(5-3)
+
+##import this
+'''The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
